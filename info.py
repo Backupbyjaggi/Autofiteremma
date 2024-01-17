b74768228982362f74c8f455fa64a22681ab33f5
Backupbyjaggi <109049575+Backupbyjaggi@users.noreply.github.com>
2024-01-17 12:58:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,7 +81,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Jaggi_movies')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/BackupByJaggi')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Jaggi_movies')
 FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Jaggi_movies')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/Jaggi_movies")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Jaggi_movies")
